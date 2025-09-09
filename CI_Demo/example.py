9654c7c723c16e3049843d36fef90ee167a5125c
Piyush singh <152459291+PiyushVIT346@users.noreply.github.com>
2025-09-10 01:00:56 +0530

    Add files via upload

---
+++ CI_Demo/example.py
@@ -0,0 +1,22 @@
+def greet(name):
+    """Prints a greeting message."""
+    print(f"Hello {name}")
+
+
+def add(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+
+def mul(a, b):
+    """Returns the product of two numbers."""
+    return a * b
+
+
+greet("Anil")
+print(add(2, 3))
+print(mul(2, 3))
+
+if __name__ == "__main__":
+    print(add(3, 5))
+    print(mul(2, 3))
